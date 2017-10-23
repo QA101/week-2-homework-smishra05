4d65bfafaf2bf687db15dd4aa28fecd68e5d8618
smishra05 <32606987+smishra05@users.noreply.github.com>
2017-10-22 19:53:43 -0700

    Add files via upload

---
+++ src/ArrayAverage(modified).java
@@ -0,0 +1,64 @@
+
+
+import java.util.Scanner;
+
+
+/**
+ * Write a class that will get input from the user through the terminal
+ * Only int will be passed in through the terminal. Any other input will likely fail.
+ * 
+ * This class will put all the ints into an array then average them together.
+ * The average will be printed to the console.
+ * 
+ * @author dustin
+ *
+ */
+public class ArrayAverage {
+
+	/**
+	 * The main method will read from the console to first determine the size of the array
+	 * 
+	 * Then, the main method should cycle through input from the user to populate the array
+	 * 
+	 * Then, the main method should average the populated array
+	 * 
+	 * Finally, the main method should print the average
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int ArraySize;
+		double average = 0;
+
+		//Creating a scanner to read input from console
+		Scanner console = new Scanner(System.in);
+		System.out.println("How many ints to average?");
+		ArraySize = console.nextInt();
+		int[] Array = new int[ArraySize];
+		
+		// TODO -- define an array
+				
+		// TODO -- write a for loop here to ask the user for input.
+
+		//for(something){
+		//
+		//}
+		 for (int i=0;i<ArraySize;i++)
+		    {
+		    	Array[i]=console.nextInt();
+		    }
+		// TODO -- average across the array
+		//average = ???; 
+		 int sum = 0;
+		 for (int i=0;i<ArraySize;i++)
+		    {
+		    	sum = sum + Array[i];
+		    }
+			average = sum/ArraySize;
+		System.out.println(average);
+		
+		//Closing the scanner
+		console.close();
+	}
+
+}
